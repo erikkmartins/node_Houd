60d1671289f038d368f8c4ec2dc4d8ca6a176465
Erik Martins Barbosa <RM70763@fiap.com.br>
2016-09-12 20:09:39 -0300

    Deus me ajude

--- app.js
+++ app.js
@@ -1,14 +1,41 @@
 //  OpenShift sample Node application
-var express = require('express')
-    app     = express();
+var express = require('express'), 
+    bodyParser = require('body-parser'),
+    app = express(),
+    router = require('./router'),
+    http = require('http');
     
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
+
+/*Conexão MySQL*/
+
+var connection = require('express-myconnection'),
+    mysql = require('mysql');
+
+app.use(
+
+    connection(mysql, {
+        host: '10.1.39.13',
+        user: 'userKIW',
+        password: 'dGBvVUKdQyNf7k7S',
+        port: '3306',
+        database: 'houd'
+    }, 'request')
+
+);
+
+
+app.use('/', router);
+
 
 // error handling
 app.use(function(err, req, res, next){
