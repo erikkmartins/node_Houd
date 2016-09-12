bdedf65e8329803b4e5227c51c150553099cae6a
Erik Martins Barbosa <RM70763@fiap.com.br>
2016-09-12 19:21:44 -0300

    Vai cavalo!

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
-var express = require('express');
-var app = express();
+//  OpenShift sample Node application
+var express = require('express')
+    app     = express();
+    
+
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
+    mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
+    mongoURLLabel = "";
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
- 
-app.listen(server_port, server_ip_address, function () {
-  console.log( "Listening on " + server_ip_address + ", server_port " + server_port )
+// error handling
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.status(500).send('Something bad happened!');
 });
 
+app.listen(port, ip);
+console.log('Server running on http://%s:%s', ip, port);
 
+module.exports = app ;
