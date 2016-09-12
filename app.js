3ac26c6ec801665c41f415ed35cdacdb0902d1c9
erikkmartins <embmister@gmail.com>
2016-09-12 11:27:34 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,39 @@
-{
-  "name": "node_Houd_ws",
-  "description": "Houd Webservice",
-  "version": "1.0.0",
-  "private": true,
-  "main": "app.js",
-  "dependencies": {
-    "body-parser": "1.9.0",
-    "express": "4.9.5",
-    "express-myconnection": "*",
-    "validator": "^5.2.0",
-    "mysql": "*"
-  },
-  "scripts": {
-    "start": "app.js"
-}
-}
+var express = require('express'), 
+    bodyParser = require('body-parser'),
+    app = express(),
+    router = require('./router'),
+    http = require('http');
+
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
+
+
+/*Conexão MySQL*/
+
+var connection = require('express-myconnection'),
+    mysql = require('mysql');
+
+app.use(
+
+    connection(mysql, {
+        host: 'localhost',
+        user: 'root',
+        password: 'root',
+        port: '8889',
+        database: 'houd'
+    }, 'request')
+
+);
+
+app.use('/', router);
+
+//start Server
+var server = app.listen(8080, function() {
+
+    console.log("Ouvindo na porta %s", server.address().port);
+
+});
