83c3a835c9b0fd853f23356233d345cc8b0c0e41
Erik Martins Barbosa <RM70763@fiap.com.br>
2016-09-12 19:03:32 -0300

    Commit

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 3000!');
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+ 
+app.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", server_port " + server_port )
 });
+
+
