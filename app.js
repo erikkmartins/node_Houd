7c2bbb9c5761bae9352d72edf77ccc78ef96caf7
Erik Martins <erik.kire@outlook.com>
2016-09-14 15:18:34 -0300

    no message

--- app.js
+++ app.js
@@ -24,14 +24,26 @@ var connection = require('express-myconnection'),
 app.use(
 
     connection(mysql, {
-        host: '10.1.39.13',
-        user: 'userKIW',
-        password: 'dGBvVUKdQyNf7k7S',
-        port: '3306',
+        host: 'localhost',
+        user: 'root',
+        password: 'root',
+        port: '8889',
         database: 'houd'
     }, 'request')
 
 );
+
+// app.use(
+
+//     connection(mysql, {
+//         host: '10.1.39.13',
+//         user: 'userKIW',
+//         password: 'dGBvVUKdQyNf7k7S',
+//         port: '3306',
+//         database: 'houd'
+//     }, 'request')
+
+// );
 
 
 app.use('/', router);
