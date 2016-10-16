67476acfd1766d95632ff0c665e418c2d921337a
Erik Martins <erik.kire@outlook.com>
2016-10-16 14:51:27 -0200

    Port change;

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var express = require('express'),
     http = require('http');
     
 
-var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 7171,
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 
